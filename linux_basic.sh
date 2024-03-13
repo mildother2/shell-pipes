a2d1a822a3e9bf3419700afe036acd9215781f30
mildother2 <1_2talk@proton.me>
2024-03-13 06:08:44 -0700

    just an echo

---
+++ linux_basic.sh
@@ -0,0 +1,28 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "#!/usr/bin/bash\n",
+    "\n",
+    "echo \"help me world\""
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.11.8"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
